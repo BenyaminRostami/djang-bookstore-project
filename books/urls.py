04945d376e4def4134ec1cf37a915973b383c32a
Benyamin <benyaminrostami85@gmail.com>
2023-03-31 11:39:31 -0700

    for fixed bug this project to suppurter

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns= [
     path ('',BookListViews.as_view() , name = 'book_list') ,
-    path('<int:pk>/' ,BookDetailView.as_view() , name = "book_detail")
+    path('<int:pk>/' ,BookDetailView.as_view() , name = "book_detail"),
+    path ('create/' , BookCreateView.as_view() , name= "book_create")
 
 ]
--- books/views.py
+++ books/views.py
@@ -8,3 +8,9 @@ class BookListViews(generic.ListView) :
 class BookDetailView(generic.DetailView):
     model=Book
     template_name = 'books/book_detail.html'
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title' , 'discription' , 'auther' , 'price']
+    template_name = 'books/book_create.html'
+
+
