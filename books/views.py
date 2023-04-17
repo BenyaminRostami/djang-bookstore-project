a301a2432f7f485cf38994882aaca8bf719e7fb1
Benyamin <benyaminrostami85@gmail.com>
2023-04-16 20:05:32 -0700

    add page ination to book list view

--- books/views.py
+++ books/views.py
@@ -3,11 +3,13 @@ from .models import Book
 from django.urls import reverse_lazy
 class BookListViews(generic.ListView) :
     model = Book
+    paginate_by = 6
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 # Create your views here.
 class BookDetailView(generic.DetailView):
     model=Book
+
     template_name = 'books/book_detail.html'
 class BookCreateView(generic.CreateView):
     model = Book
